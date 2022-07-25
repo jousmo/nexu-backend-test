820f9ee39d09d7418e6d5610df79c17a30914f14
José Uscanga <jousmo@outlook.com>
2022-07-25 18:52:45 -0500

    feat: implement middlewares not found and error

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const server = express()
 const routes = require('./routes')
+const { notFoundHandler, logErrors, errorHandler } = require('./middlewares')
 const port = 3000
 
 server.use(cors())
@@ -10,5 +11,8 @@ server.use(helmet())
 server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
 server.use('/api/v1', routes)
+server.use(notFoundHandler)
+server.use(logErrors)
+server.use(errorHandler)
 
 server.listen(port, () => console.log(`Server running in port ${port}`))
