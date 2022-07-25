47204315c8ddc14614c8ab583614017381ae4fcd
José Uscanga <jousmo@outlook.com>
2022-07-25 18:53:52 -0500

    feat: helpers for services with mock db

---
+++ helpers/index.js
@@ -0,0 +1,17 @@
+const searchModelsByBrandId = (id, schema) => schema.find(el => el.brandId === +id)
+
+const searchName = (name, schema) => schema.find(el => el.name.toLowerCase() === name.toLowerCase())
+
+const calculateId = schema => schema.length ? schema.at(-1).id + 1 : 1
+
+const searchById = (id, schema) => schema.find(el => el.id === +id)
+
+const searchIndex = (id, schema) => schema.findIndex(el => el.id === +id)
+
+module.exports = {
+  searchName,
+  searchById,
+  searchModelsByBrandId,
+  calculateId,
+  searchIndex
+}
