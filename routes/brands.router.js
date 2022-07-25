90fd376a066d3b98dced28b8fa2b50a2544cc15d
José Uscanga <jousmo@outlook.com>
2022-07-25 18:54:35 -0500

    feat: update logic routes use services

--- routes/brands.router.js
+++ routes/brands.router.js
@@ -1,20 +1,30 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler')
+const { brandsServices } = require('../services')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json({ ok: true })
-})
+router.get('/', asyncHandler(async (req, res) => {
+  const brands = await brandsServices.findBrands()
+  res.status(200).json(brands)
+}))
 
-router.get('/:id/models', (req, res) => {
-  res.json({ ok: true })
-})
+router.get('/:id/models', asyncHandler(async (req, res) => {
+  const { id } = !!req.params && req.params
+  const brand = await brandsServices.findModelsByBrandId(id)
+  res.status(200).json(brand)
+}))
 
-router.post('/', (req, res) => {
-  res.json({ ok: true })
-})
+router.post('/', asyncHandler(async (req, res) => {
+  const { body } = !!req && req
+  const newBrand = await brandsServices.createBrand(body)
+  res.status(201).json(newBrand)
+}))
 
-router.post('/:id/models', (req, res) => {
-  res.json({ ok: true })
-})
+router.post('/:id/models', asyncHandler(async (req, res) => {
+  const { params, body } = !!req && req
+  const { id } = !!params && params
+  const newModel = await brandsServices.createModelByBrandId(id, body)
+  res.status(201).json(newModel)
+}))
 
 module.exports = router
--- routes/models.router.js
+++ routes/models.router.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler')
+const { modelsServices } = require('../services')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.json({ ok: true })
-})
+router.get('/', asyncHandler(async (req, res) => {
+  const { query } = !!req && req
+  const models = await modelsServices.findModels(query)
+  res.status(200).json(models)
+}))
 
-router.put('/:id', (req, res) => {
-  res.json({ ok: true })
-})
+router.put('/:id', asyncHandler(async (req, res) => {
+  const { body, params } = !!req && req
+  const { id } = !!params && params
+
+  const updateModel = await modelsServices.update(id, body)
+  res.status(200).json(updateModel)
+}))
 
 module.exports = router
