6d43f13732957b95e566e77c15b817d9bbb25fd8
José Uscanga <jousmo@outlook.com>
2022-07-24 20:05:50 -0500

    feat: implement basic middlewares

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
 const server = express()
 const routes = require('./routes')
 const port = 3000
 
+server.use(cors())
+server.use(helmet())
+server.use(express.json())
+server.use(express.urlencoded({ extended: false }))
 server.use('/api/v1', routes)
 
 server.listen(port, () => console.log(`Server running in port ${port}`))
