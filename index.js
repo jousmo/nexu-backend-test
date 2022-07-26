8109bac14174f0209164d76523d2e214d31e9b4d
José Uscanga <jousmo@outlook.com>
2022-07-26 16:13:27 -0500

    feat: get port and environment for config app in running server

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 const server = express()
 const routes = require('./routes')
 const { notFoundHandler, logErrors, errorHandler } = require('./middlewares')
-const port = 3000
+const { PORT, ENVIRONMENT } = require('./config')
 
 server.use(cors())
 server.use(helmet())
@@ -15,4 +16,8 @@ server.use(notFoundHandler)
 server.use(logErrors)
 server.use(errorHandler)
 
-server.listen(port, () => console.log(`Server running in port ${port}`))
+if (ENVIRONMENT !== 'test') {
+  server.listen(PORT, () => console.log(`Server running in port ${PORT}`))
+} else {
+  module.exports = server
+}
