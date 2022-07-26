a42c8ee851c6b0f7ac633b23dc64a9a15591dab6
José Uscanga <jousmo@outlook.com>
2022-07-26 14:46:55 -0500

    feat: update not found handler use boom

--- middlewares/notFound.handler.js
+++ middlewares/notFound.handler.js
@@ -1,1 +1,8 @@
-module.exports = (req, res) => res.status(404).json({ message: 'resource not found' })
+const boom = require('@hapi/boom')
+
+function notFoundHandler (req, res) {
+  const { output } = boom.notFound('resource not found')
+  res.status(output.statusCode).json(output.payload)
+}
+
+module.exports = notFoundHandler
