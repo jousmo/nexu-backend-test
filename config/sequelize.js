d9297ed8df8bc41bd844500eab44080c14ebeb86
José Uscanga <jousmo@outlook.com>
2022-07-27 18:59:24 -0500

    feat: update env vars and connection sequelize

--- config/sequelize.js
+++ config/sequelize.js
@@ -6,6 +6,7 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
-    logging: true
+    logging: process.env.DB_LOGGING,
+    seederStorage: process.env.DB_SEEDER_STORAGE
   }
 }
