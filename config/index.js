f4dfaa178415917a707c21189ba7883795861076
José Uscanga <jousmo@outlook.com>
2022-07-27 17:51:10 -0500

    feat: update config vars separate server and sequelize

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-module.exports = {
-  PORT: process.env.NODE_PORT,
-  ENVIRONMENT: process.env.NODE_ENV
-}
+const server = require('./server')
+const sequelize = require('./sequelize')
+
+module.exports = { server, sequelize }
