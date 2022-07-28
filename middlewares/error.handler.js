454e9dbb887c450b1f257e602c66a40d9107f63d
José Uscanga <jousmo@outlook.com>
2022-07-28 07:34:58 -0500

    feat: separate logs error

--- middlewares/error.handler.js
+++ middlewares/error.handler.js
@@ -1,4 +1,5 @@
 const boom = require('@hapi/boom')
+const { ValidationError } = require('sequelize')
 const { ENVIRONMENT } = require('../config/server')
 
 function logErrors (err, req, res, next) {
@@ -9,21 +10,41 @@ function logErrors (err, req, res, next) {
   next(err)
 }
 
-function errorHandler (err, req, res, next) {
-  let error
-
-  if (!err.isBoom) {
-    const { output } = boom.badImplementation()
-    error = output
-  } else {
+function boomErrorHandler (err, req, res, next) {
+  if (err.isBoom) {
     const { output } = err
-    error = output
+    return res.status(output.statusCode).json(output.payload)
   }
 
-  res.status(error.statusCode).json(error.payload)
+  next(err)
+}
+
+function ormErrorHandler (err, req, res, next) {
+  if (err instanceof ValidationError) {
+    const { output } = boom.conflict(err.name)
+    const newOutput = {
+      ...output.payload,
+      errors: err.errors
+    }
+
+    return res.status(output.statusCode).json(newOutput)
+  }
+
+  next(err)
+}
+
+function errorHandler (err, req, res, next) {
+  if (err) {
+    const { output } = boom.badImplementation()
+    return res.status(output.statusCode).json(output.payload)
+  }
+
+  next()
 }
 
 module.exports = {
   logErrors,
+  boomErrorHandler,
+  ormErrorHandler,
   errorHandler
 }
