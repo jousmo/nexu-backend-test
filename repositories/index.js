e81e679b76db93d4057fe9779dead39a6816c952
José Uscanga <jousmo@outlook.com>
2022-07-27 20:45:41 -0500

    feat: implement repositories for brands and models

---
+++ repositories/index.js
@@ -0,0 +1,7 @@
+const brandsMockRepository = require('./brands/brands.mock.repository')
+const modelsMockRepository = require('./models/models.mock.repository')
+
+module.exports = {
+  brandsMockRepository,
+  modelsMockRepository
+}
