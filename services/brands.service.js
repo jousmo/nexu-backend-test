6f7762b557d832dccd5a68af89bb23ba843f918c
José Uscanga <jousmo@outlook.com>
2022-07-27 20:46:53 -0500

    feat: use dependencies injections in the services to use brands and model repositories

--- services/brands.service.js
+++ services/brands.service.js
@@ -1,67 +1,24 @@
-const boom = require('@hapi/boom')
-const { brands, models } = require('../lib/mock-db')
-const { searchModelsByBrandId, searchName, calculateId, searchIndex } = require('../helpers')
-
-async function findBrands () {
-  return brands || []
-}
-
-async function findModelsByBrandId (id) {
-  const brand = searchModelsByBrandId(id, models)
-
-  if (!brand) {
-    throw boom.notFound('brand not found')
+module.exports = brandsRepository => {
+  async function findBrands () {
+    return await brandsRepository.findBrands()
   }
 
-  return brand
-}
-
-async function createBrand (data) {
-  const { name } = data
-  const nameExists = searchName(name, brands)
-
-  if (nameExists) {
-    throw boom.conflict('name brand exist')
+  async function findModelsByBrandId (id) {
+    return await brandsRepository.findModelsByBrandId(id)
   }
 
-  const id = calculateId(brands)
-  const newBrand = { id, average_price: 0, ...data }
-  brands.push(newBrand)
-
-  return newBrand
-}
-
-async function createModelByBrandId (brandId, data) {
-  const { average_price } = !!data && data
-  const brandIndex = searchIndex(brandId, brands)
-
-  if (brandIndex === -1) {
-    throw boom.notFound('brand not exist')
+  async function createBrand (data) {
+    return await brandsRepository.createBrand(data)
   }
 
-  const { name } = data
-  const nameExists = searchName(name, models)
-
-  if (nameExists) {
-    throw boom.conflict('name model exist')
+  async function createModelByBrandId (brandId, data) {
+    return await brandsRepository.createModelByBrandId(brandId, data)
   }
 
-  // Create model
-  const id = calculateId(models)
-  const newModel = { id, brandId: +brandId, ...data }
-  models.push(newModel)
-
-  // Update average_price in brand
-  const brand = brands[brandIndex]
-  const newAveragePrice = (brand?.average_price || 0) + (average_price || 0)
-  brands[brandIndex] = { ...brand, average_price: newAveragePrice }
-
-  return newModel
-}
-
-module.exports = {
-  findBrands,
-  findModelsByBrandId,
-  createBrand,
-  createModelByBrandId
+  return {
+    findBrands,
+    findModelsByBrandId,
+    createBrand,
+    createModelByBrandId
+  }
 }
