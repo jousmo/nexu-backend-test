34fa617d0399c60cc6802939bd1cbeaa119db477
José Uscanga <jousmo@outlook.com>
2022-07-28 07:37:43 -0500

    fix: export repositories

--- repositories/index.js
+++ repositories/index.js
@@ -1,7 +1,9 @@
 const brandsMockRepository = require('./brands/brands.mock.repository')
+const brandsSequelizeRepository = require('./brands/brands.sequelize.repository')
 const modelsMockRepository = require('./models/models.mock.repository')
 
 module.exports = {
   brandsMockRepository,
+  brandsSequelizeRepository,
   modelsMockRepository
 }
