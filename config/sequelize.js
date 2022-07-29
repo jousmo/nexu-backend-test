d1e6c62e42bc12840faa5cc895265fd6bfded1ca
José Uscanga <jousmo@outlook.com>
2022-07-29 00:57:45 -0500

    feat: include test config for sequelize

--- config/sequelize.js
+++ config/sequelize.js
@@ -8,5 +8,15 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
     logging: process.env.DB_LOGGING,
     seederStorage: process.env.DB_SEEDER_STORAGE
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: process.env.DB_LOGGING,
+    seederStorage: process.env.DB_SEEDER_STORAGE
   }
 }
