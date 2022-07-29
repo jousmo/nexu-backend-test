98f6d6c41b28b1668a8ef1e61cc7884a5a5e98d9
José Uscanga <jousmo@outlook.com>
2022-07-29 00:55:52 -0500

    fix: remove id for seeders

--- db/seeders/20220727230449-brands.js
+++ db/seeders/20220727230449-brands.js
@@ -7,11 +7,11 @@ module.exports = {
     const date = dayjs('2022-07-27').utc().format()
 
     await queryInterface.bulkInsert('brands', [
-      { id: 1, name: 'Acura', average_price: 375684.5, createdAt: date, updatedAt: date },
-      { id: 2, name: 'Audi', average_price: 3818225, createdAt: date, updatedAt: date },
-      { id: 3, name: 'Bentley', average_price: 395753, createdAt: date, updatedAt: date },
-      { id: 4, name: 'BMW', average_price: 239050, createdAt: date, updatedAt: date },
-      { id: 5, name: 'Buick', average_price: 239050, createdAt: date, updatedAt: date }
+      { name: 'Acura', average_price: 375684.5, createdAt: date, updatedAt: date },
+      { name: 'Audi', average_price: 3818225, createdAt: date, updatedAt: date },
+      { name: 'Bentley', average_price: 395753, createdAt: date, updatedAt: date },
+      { name: 'BMW', average_price: 239050, createdAt: date, updatedAt: date },
+      { name: 'Buick', average_price: 239050, createdAt: date, updatedAt: date }
     ], {})
   },
 
--- db/seeders/20220727233705-models.js
+++ db/seeders/20220727233705-models.js
@@ -7,12 +7,12 @@ module.exports = {
     const date = dayjs('2022-07-27').utc().format()
 
     await queryInterface.bulkInsert('models', [
-      { id: 1, name: 'ILX', average_price: 303176, brandId: 1, createdAt: date, updatedAt: date },
-      { id: 2, name: 'MDX', average_price: 448193, brandId: 1, createdAt: date, updatedAt: date },
-      { id: 3, name: 'NSX', average_price: 3818225, brandId: 2, createdAt: date, updatedAt: date },
-      { id: 4, name: 'RDX', average_price: 395753, brandId: 3, createdAt: date, updatedAt: date },
-      { id: 5, name: 'RL', average_price: 239050, brandId: 4, createdAt: date, updatedAt: date },
-      { id: 6, name: 'OS', average_price: 239050, brandId: 5, createdAt: date, updatedAt: date }
+      { name: 'ILX', average_price: 303176, brandId: 1, createdAt: date, updatedAt: date },
+      { name: 'MDX', average_price: 448193, brandId: 1, createdAt: date, updatedAt: date },
+      { name: 'NSX', average_price: 3818225, brandId: 2, createdAt: date, updatedAt: date },
+      { name: 'RDX', average_price: 395753, brandId: 3, createdAt: date, updatedAt: date },
+      { name: 'RL', average_price: 239050, brandId: 4, createdAt: date, updatedAt: date },
+      { name: 'OS', average_price: 239050, brandId: 5, createdAt: date, updatedAt: date }
     ], {})
   },
 
